69b22bf4311becb1dce9e6346911219dac6807ff
Easlee Liu <easlee@outlook.com>
2019-04-21 23:03:41 +0800

    Import

---
+++ unitysln/Reporter/Assets/OMO/SDK/NET/Sample/Sample.cs
@@ -0,0 +1,117 @@
+﻿using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using OMO.SDK.NET;
+
+public class Sample : MonoBehaviour
+{
+
+    public Text text;
+    public InputField inputReport;
+    public Button btnRun;
+    public Button btnStop;
+
+    private Reporter reporter = null;
+
+    private byte[] data { get; set; }
+    private Queue<string> log = new Queue<string>();
+    private StringBuilder sb = new StringBuilder();
+
+    private float timer = 0f;
+
+    private float second = 0f;
+
+
+    void Awake()
+    {
+        addLog("run reporter");
+        inputReport.text = "127.0.0.1:18999";
+        btnStop.gameObject.SetActive(false);
+    }
+
+    void Start()
+    {
+        ProtocolJSON.Request req = new ProtocolJSON.Request();
+        req.head.msg = "ping";
+        string json = ProtocolJSON.RequestToJSON(req);
+        data = System.Text.Encoding.UTF8.GetBytes(json);
+
+        btnRun.onClick.AddListener(() =>
+        {
+            btnRun.gameObject.SetActive(false);
+            btnStop.gameObject.SetActive(true);
+            reporter = ReporterMgr.NewReporter(string.Format("{0}", Time.deltaTime));
+            reporter.Setup(inputReport.text.Split(':')[0], int.Parse(inputReport.text.Split(':')[1]));
+            reporter.onReply = this.OnReply;
+            reporter.onException = this.OnException;
+            reporter.Run();
+        });
+
+        btnStop.onClick.AddListener(() =>
+        {
+            btnRun.gameObject.SetActive(true);
+            btnStop.gameObject.SetActive(false);
+            if (null != reporter)
+            {
+                reporter.Stop();
+                reporter = null;
+            }
+        });
+    }
+
+    void Update()
+    {
+        text.text = printLog();
+
+        if (timer > 1.0f)
+        {
+            second += 1f;
+            if (null != reporter)
+            {
+                addLog("ping ... ");
+                reporter.Report(data);
+            }
+            timer = 0f;
+        }
+        timer += Time.deltaTime;
+    }
+
+    void OnDisable()
+    {
+        if (null != reporter)
+        {
+            reporter.Stop();
+        }
+    }
+
+    void OnReply(byte[] _data)
+    {
+        string reply = System.Text.Encoding.UTF8.GetString(_data);
+        addLog(reply);
+    }
+
+    void OnException(System.Exception ex)
+    {
+        addLog(ex.Message);
+    }
+
+    void addLog(string _msg)
+    {
+        log.Enqueue(string.Format("{0} - {1}", second, _msg));
+        if (log.Count > 20)
+            log.Dequeue();
+    }
+
+    string printLog()
+    {
+        sb.Remove(0, sb.Length);
+        foreach(string msg in log)
+        {
+            sb.AppendLine(msg);
+        }
+        return sb.ToString();
+    }
+
+}
---
+++ unitysln/Reporter/Assets/OMO/SDK/NET/Scripts/Reporter.cs
@@ -0,0 +1,156 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace OMO.SDK.NET
+{
+    public class Reporter
+    {
+        public delegate void OnReplyDelegate(byte[] _reply);
+        public delegate void OnExceptionDelegate(System.Exception _ex);
+
+        public OnReplyDelegate onReply;
+        public OnExceptionDelegate onException;
+
+        private string host;
+        private int port;
+        private bool running_ = false;
+        private UdpClient udpClient = null;
+        private IPEndPoint remoteEP = null;
+
+        private Queue<byte[]> outPool = new Queue<byte[]>();
+
+        public void Setup(string _host, int _port)
+        {
+            host = _host;
+            port = _port;
+            remoteEP = new IPEndPoint(IPAddress.Parse(host), port);
+            //auto allocate port
+            udpClient = new UdpClient(new IPEndPoint(IPAddress.Any, 0));
+        }
+
+        public void Run()
+        {
+            if (!running_)
+            {
+                running_ = true;
+
+                Thread receiverThread = new Thread(runReceiver);
+                receiverThread.Start();
+                Thread senderThread = new Thread(runSender);
+                senderThread.Start();
+            }
+        }
+
+        public void Stop()
+        {
+            if (running_)
+            {
+                running_ = false;
+                udpClient.Close();
+                udpClient = null;
+            }
+        }
+
+        public void Report(byte[] _data)
+        {
+            outPool.Enqueue(_data);
+        }
+
+        private void runReceiver()
+        {
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            while (running_)
+            {
+                if (null == udpClient)
+                    break;
+                try
+                {
+                    if (udpClient.Available <= 0)
+                        continue;
+                    byte[] buf = udpClient.Receive(ref sender);
+                    if (null == buf)
+                        continue;
+                    if (null != onReply) onReply(buf);
+                }
+                catch (System.Exception ex)
+                {
+                    if (null != onException) onException(ex);
+                }
+            }
+            Debug.Log("Reporter::receiver - thread exit");
+        }
+
+        private void runSender()
+        {
+            while (running_)
+            {
+                if (null == udpClient)
+                    break;
+                while (outPool.Count != 0)
+                {
+                    try
+                    {
+                        byte[] buf = outPool.Dequeue();
+                        if (null == buf)
+                            continue;
+                        //must use endPoint
+                        udpClient.Send(buf, buf.Length, remoteEP);
+                    }
+                    catch (System.Exception ex)
+                    {
+                        if (null != onException) onException(ex);
+                    }
+                }
+                Thread.Sleep(50);
+            }
+            Debug.Log("Reporter::sender - thread exit");
+        }
+    }
+
+    public static class ReporterMgr
+    {
+        private static Dictionary<string, Reporter> reporters = new Dictionary<string, Reporter>();
+
+        public static Reporter NewReporter(string _name)
+        {
+            if (reporters.ContainsKey(_name))
+                return null;
+
+            Reporter reporter = new Reporter();
+            reporters.Add(_name, reporter);
+            return reporter;
+        }
+
+        public static void DeleteReporter(string _name)
+        {
+            if (!reporters.ContainsKey(_name))
+                return;
+            reporters.Remove(_name);
+        }
+
+        public static Reporter Find(string _name)
+        {
+            if (!reporters.ContainsKey(_name))
+                return null;
+
+            return reporters[_name];
+        }
+
+        public static void Run()
+        {
+            foreach (Reporter reporter in reporters.Values)
+                reporter.Run();
+        }
+
+        public static void Stop()
+        {
+            foreach (Reporter reporter in reporters.Values)
+                reporter.Stop();
+        }
+    }
+}//XLobby.Manager
